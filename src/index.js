24f0846480b128612124d8faf32a0a62e45fe786
v1tal4ik <vetalgta@gmail.com>
2018-11-06 14:30:01 +0200

    v.4.2

--- src/index.js
+++ src/index.js
@@ -25,6 +25,7 @@ function createDivWithText(text) {
    prepend(document.querySelector('#one'), document.querySelector('#two')) // добавит элемент переданный первым аргументом в начало элемента переданного вторым аргументом
  */
 function prepend(what, where) {
+     where.insertBefore(what,where.firstChild);
 }
 
 /*
